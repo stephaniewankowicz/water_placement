c43f36c6b200dc529e9f3080413d95403e621b75
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-12-02 16:20:13 -0800

    Update ML_detection.py

--- analysis/ML_detection.py
+++ analysis/ML_detection.py
@@ -24,3 +24,20 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+
+
+import numpy as np
++import matplotlib.pyplot as plt
++
++#define the function
++def likelihood(x,y,x_array,y_array,sigma):
++    #define the likelihood function
++    #x and y are the coordinates of the point we are testing
++    #x_array and y_array are the coordinates of the points we are using to test
++    #sigma is the standard deviation of the gaussian
++    #returns the likelihood of the point existing
++    #the likelihood is the sum of the gaussian of each point in the array
++    #the gaussian is defined as 1/(sigma*sqrt(2*pi))*exp(-(x-x_array[i])**2/(2*sigma**2))
++    #the sum is over all the points in the array
++    #the likelihood is the product of the gaussians
++    #the product is over all the points in the array
